f6aea8ba0cf62839b6eca549af50461ad91c19a6
Ubuntu <ubuntu@ip-172-31-27-73.ec2.internal>
2019-09-13 18:26:25 +0000

    area/perimeter calculation program

---
+++ rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Jacob Bonner
+# Created on: September 2019
+# This program can calculate the area and perimeter of a rectangle
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("The perimeter is {}cm".format(5*2+3*2))
+    print("The area is {}cm^2".format(5*3))
+
+
+if __name__ == "__main__":
+    main()
